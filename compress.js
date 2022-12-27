bbcda077acf59e0036906f62e3a43ab80111e689
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-12-27 17:00:52 -0300

    first commit

---
+++ compress.js
@@ -0,0 +1,20 @@
+const compress_images = require("compress-images");
+ 
+
+//INPUT_path_to_your_images = "./invitadisima/assets/landing/**/*.{png,PNG}";
+INPUT_path_to_your_images = "./entrada/**";
+OUTPUT_path = "./salida/";
+
+compress_images(INPUT_path_to_your_images, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+        { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+        { svg: { engine: "svgo", command: "--multipass" } },
+        { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+  function (error, completed, statistic) {
+    console.log("-------------");
+    console.log(error);
+    console.log(completed);
+    console.log(statistic);
+    console.log("-------------");
+  }
+);
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Pasos para ejecutar
+
+1) ejecutar npm install para instalar dependencias 
+2) Colocar en la carpeta "entrada" las imgs a optimizar
+3) En la consola, ubicado en el directorio raíz, ejecutar node compress.js . Las imágenes optimizadas se almacenan en la carpeta de "salida". En caso de haber error, el mismo se mostrará en consola.
